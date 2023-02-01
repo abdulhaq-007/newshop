69aeb86b159152d0543260a2deed3199d0976988
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2023-02-01 12:37:32 -0800

    updated project stopped in models accounts

---
+++ shop/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+]
